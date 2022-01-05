db92c629358a6be6f28fa583bb7b1dfb9c3b4a4a
ali <ali2@bookingvila.com>
2022-01-05 12:29:27 +0330

    inherit

---
+++ chapter1/inherit.sol
@@ -0,0 +1,8 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.8.0 <0.9.0;
+
+import "./function.sol";
+
+contract SecContract is FirstContract {
+    // this contract have all function and property of fistCOntract
+}
